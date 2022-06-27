b5b638d3bbd1bdd867f75992c43984d5490969fd
JYL <jyl7094@gmail.com>
2022-06-27 02:30:18 -0400

    connect to mongoDb

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
-const path = require('path');
+require('dotenv').config();
 
-const express = require('express');
-const bodyParser = require('body-parser');
+const path = require("path");
 
-const indexRoutes = require('./routes/index');
-const mcdonaldsRoutes = require('./routes/mcdonalds');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const indexRoutes = require("./routes/index");
+const mcdonaldsRoutes = require("./routes/mcdonalds");
 // const pandaExpressRoutes = require('./routes/panda-express');
 
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/mcdonalds', mcdonaldsRoutes);
+app.use("/mcdonalds", mcdonaldsRoutes);
 // app.use('/panda-express', pandaExpressRoutes);
 app.use(indexRoutes);
+
+mongoose.connect(process.env.MONGODB).catch((err) => {
+  console.log(err);
+});
 
 app.listen(process.env.PORT || 3000);
